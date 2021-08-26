bc2efd1558c483735cb4bcc9412c3b79766063d4
Nirmala Luwang <nirmalaluwang@gmail.com>
2021-08-26 10:25:43 +0530

    seeds jumbotron commit

---
+++ netflix-clone-project/src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { JumbotronContainer } from '../containers/jumbotron';
+
+
+ function Home() {
+    return (
+        <JumbotronContainer />    
+    );
+}
+
+export default Home
+
