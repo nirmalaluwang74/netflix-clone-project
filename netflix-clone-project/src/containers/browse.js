e1d120eac3760c2fcc60f04a2281170abea06260
Nirmala Luwang <nirmalaluwang@gmail.com>
2021-09-12 12:39:19 +0530

    card

--- netflix-clone-project/src/containers/browse.js
+++ netflix-clone-project/src/containers/browse.js
@@ -76,27 +76,23 @@ export function BrowseContainer({ slides }) {
                 </Header.Feature>
             </Header>
 
-
             <Card.Group>
                 {slideRows.map((slideItem) => (
                     <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
                         <Card.Title>{slideItem.title}</Card.Title>
                         <Card.Entities>
-
-
-                            
+                            {slideItem.data.map(() => (
+                                <Card.Item key={item.docId} item={item}> 
+                                    <Card.Image src= {`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Meta>
+                                </Card.Item>
+                            ))}
                         </Card.Entities>
-                
-                
-                
-                
-                
-                )}
-
-
-
-
-
+                    </Card>    
+                ))}
             </Card.Group>
                 <FooterContainer />
             </>)
