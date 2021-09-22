4f23f22e419349de5fc75681417eebcddaffd0cb
Nirmala Luwang <nirmalaluwang@gmail.com>
2021-09-22 08:38:24 +0530

    header

--- netflix-clone-project/src/containers/header.js
+++ netflix-clone-project/src/containers/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Header } from '../components';
-// import * as Routes from '../constants/routes';
+import * as Routes from '../constants/routes';
 
 const logo = require('/home/nirmala/code/nirmalaluwang74/netflix-clone-project/src/images/misc/logo.png')
 const backgroundImage = require('/home/nirmala/code/nirmalaluwang74/netflix-clone-project/src/images/misc/bg-movies.jpg')
@@ -9,7 +9,7 @@ export function HeaderContainer({children}) {
     return (
         <Header src={backgroundImage}>
             <Header.Frame>
-                <Header.Logo src={logo} alt='Netflix'/>
+                <Header.Logo to={Routes.home} src='../images/misc/logo.png' alt='Netflix'/>
                 <Header.ButtonLink > Sign In </Header.ButtonLink>
             </Header.Frame>
             {children}
